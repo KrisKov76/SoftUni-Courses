ee08897dec45443c6fb571e2a416211ae3d25795
Kristian Kovachev <123325623+KrisKov76@users.noreply.github.com>
2023-11-09 06:38:18 +0200

    Update

---
+++ python_OOP_10_2023/05_static_and_class+methods - Lab/04_hotel_rooms/project/hotel.py
@@ -0,0 +1,9 @@
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars_count: int):
+        return cls(f"{stars_count} stars Hotel")
